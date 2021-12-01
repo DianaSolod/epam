9bbcc2029462c001e94fe36d2b68394094bc51f1
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2021-12-02 01:23:11 +0300

    Update Trial.java

--- inheritance0/Trial.java
+++ inheritance0/Trial.java
@@ -60,9 +60,6 @@ public class Trial {
             return false;
         }
         Trial trial = (Trial) obj;
-        if (name.equals(trial.name) & mark1 == trial.mark1 & mark2 == trial.mark2) {
-            return true;
-        }
-        else return false;
+        return name.equals(trial.name) & mark1 == trial.mark1 & mark2 == trial.mark2;
     }
 }
