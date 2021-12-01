22c0ea2866b3e46f6242acab8c70c7e4e7d73ff0
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2021-12-02 01:33:53 +0300

    Update Utils.java

--- inheritance0/Utils.java
+++ inheritance0/Utils.java
@@ -5,12 +5,11 @@ public class Utils {
         }
     }
 
-    public static void failedTrials(Trial[] trials){
+    public static void badTrials(Trial[] trials){
         System.out.printf("Trials with average value of marks less then %s: %n",Trial.PASS_MARK/2);
         for (Trial trial : trials) {
             if (trial.average() < Trial.PASS_MARK / 2){
                 System.out.println(trial);
-                trial.clearMarks();
             }
         }
     }
