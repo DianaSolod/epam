dfab21fa4ea92d4fdb7df88baef8fa1fa3750cd0
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2021-12-02 01:19:16 +0300

    Update ExtraTrial.java

--- inheritance0/ExtraTrial.java
+++ inheritance0/ExtraTrial.java
@@ -5,6 +5,10 @@ public class ExtraTrial extends Trial{
     public int getMark3(){
         return mark3;
     }
+    
+    public void setMark3(int mark3) {
+        this.mark3 = mark3;
+    }
 
     public ExtraTrial(String name, int mark1, int mark2, int mark3){
         super(name, mark1, mark2);
