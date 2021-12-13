74f9019cf5c8471c353483c73c07d73a16b5a562
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2021-12-14 00:40:23 +0300

    Update Byn.java

--- inheritance2/Byn.java
+++ inheritance2/Byn.java
@@ -55,32 +55,6 @@ public class Byn implements Comparable<Byn>{
         return new Byn(value - amount.value);
     }
 
-    public enum RoundMethod {
-        FLOOR {
-            double roundFunction(double d) {
-                return Math.floor(d);
-            }
-        },
-        ROUND {
-            double roundFunction(double d) {
-                return Math.round(d);
-            }
-        },
-        CEIL {
-            double roundFunction(double d) {
-                return Math.ceil(d);
-            }
-        };
-
-        abstract double roundFunction(double value);
-        int round(double roundedValue, RoundMethod roundMethod, int d) {
-            int[] tenPowD = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
-            int tenPow = tenPowD[d];
-            int result = (int) roundMethod.roundFunction(roundedValue / tenPow) * tenPow;
-            return result;
-        }
-    }
-
     public int round(double roundedValue, RoundMethod roundMethod, int d) {
         int[] tenPowD = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
         int tenPow = tenPowD[d];
