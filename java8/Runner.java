ca64d575b3a1161fb47c33811b6db17730c6b860
DianaSolod <61231171+DianaSolod@users.noreply.github.com>
2022-01-09 23:54:46 +0300

    Add files via upload

---
+++ java8/Runner.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Runner {
+    public static void main(String[] args) {
+        ArrayList<Trial> trials = new ArrayList<Trial>(Arrays.asList(
+                new Trial("Sakovich", 45, 93),
+                new Trial("Sergeev", 55, 50),
+                new Trial("Stepanenko", 60, 90),
+                new LiteTrial("Sergeev", 29, 95),
+                new LiteTrial("Lis", 35, 68),
+                new StrongTrial("Sakovich", 90, 90),
+                new StrongTrial("Sergeev", 70, 57),
+                new ExtraTrial("Stepanenko", 40, 50, 60),
+                new ExtraTrial("Sakovich", 70, 51, 80)
+        ));
+
+        trials.forEach(System.out::println);
+
+        System.out.println("\nNumber of passed trials:" +
+                trials.stream().filter(trial -> trial.isPassed()).count());
+
+        Collections.sort(trials);
+
+        System.out.println("\nSum of marks from sorted array:");
+        trials.stream().map(Trial::getSum).forEach(System.out::println);
+
+        ArrayList<Trial> unpassed = new ArrayList<Trial>(trials.stream().filter(trial -> !trial.isPassed()).toList());
+        unpassed.forEach(Trial::clearMarks);
+        System.out.println("\nUnpassed trials with cleared marks:");
+        unpassed.forEach(System.out::println);
+
+        int sums[] = trials.stream().mapToInt(Trial::getSum).toArray();
+        System.out.println(Arrays.toString(sums));
+    }
+}
---
+++ java8/Trial.java
@@ -0,0 +1,67 @@
+public class Trial  implements Comparable <Trial>{
+    public static final int PASS_MARK = 120;
+    private String account;
+    private int mark1;
+    private int mark2;
+
+    public Trial() {
+    }
+
+    public Trial(String account, int mark1, int mark2) {
+        this.account = account;
+        this.mark1 = mark1;
+        this.mark2 = mark2;
+    }
+
+    public int getMark1() {
+        return mark1;
+    }
+    public int getMark2() {
+        return mark2;
+    }
+    public String getAccount() {
+        return account;
+    }
+    public void clearMarks(){
+        mark1 = 0;
+        mark2 = 0;
+    }
+
+    protected int result() {
+        return mark1 + mark2;
+    }
+    public boolean isPassed() {
+        return result() >= PASS_MARK;
+    }
+
+    public double average(){
+        return (double)(mark1 + mark2) / 2;
+    }
+
+    public int getSum(){
+        return mark1 + mark2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s;%d;%d;%b",
+                account, mark1, mark2, isPassed());
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Trial trial = (Trial) obj;
+        if (account.equals(trial.account) & mark1 == trial.mark1 & mark2 == trial.mark2) {
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public int compareTo(Trial o) {
+        return getSum() - o.getSum();
+    }
+}
